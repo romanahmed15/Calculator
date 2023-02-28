7638590a1fae2f13ea8464dffae9dbba0dfec5c0
Roman Ahmed <126553951+romanahmed15@users.noreply.github.com>
2023-02-28 14:56:54 +0600

    Add files via upload

---
+++ h3.py
@@ -0,0 +1,26 @@
+# calculator
+# Exercise 2 - Faulty Calculator
+# 45 * 3 = 555, 56+9 = 77, 56/6 = 4
+
+'''num1 = int(input('Enter the 1st number:'))
+num2 = int(input('Enter the 2nd number:'))
+print('what you want?'' (sum +)', '(sub -)', '(multi *)', '(dev /)')
+calculatatoin = input()
+
+
+if num1 == 45 and num2 == 3 and calculatatoin == '*':
+    print('555')
+elif num1 == 56 and num2 == 9 and calculatatoin == '+':
+    print(77)
+elif num1 == 56 and num2 == 6 and calculatatoin == '/':
+    print(4)
+elif calculatatoin == '+':
+    print('sum is:', num1+num2)
+elif calculatatoin == '-':
+    print('sub is :', num1-num2)
+elif calculatatoin == '*':
+    print('multi is :', num1*num2)
+elif calculatatoin == '/':
+    print('dev is :', num1/num2)
+else:
+    print('check your input') '''
